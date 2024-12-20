ab0a09de4848b6295430f89282ec3b761b81d38e
Erie Putranto <erieputranto@gmail.com>
2024-12-20 10:39:45 +0700

    update: page send birthday greeting in widget dan page livewire

--- app/Filament/Widgets/Birthday.php
+++ app/Filament/Widgets/Birthday.php
@@ -11,6 +11,8 @@ use Filament\Tables\Actions\Action;
 use Filament\Widgets\TableWidget as BaseWidget;
 use App\Models\BirthdayLog;
 use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\Http;
+use App\Models\BirthdayGreeting;
 
 class Birthday extends BaseWidget
 {
@@ -47,15 +49,38 @@ class Birthday extends BaseWidget
                     ->action(function (Profile $record) {
                         $this->dispatch('spin-start');
 
+                        $greeting = BirthdayGreeting::inRandomOrder()->first();
+
+                        $message = str_replace(
+                            ['{{first_name}}', '{{last_name}}', '{{birthday}}', '\n'],
+                            [$record->first_name, $record->last_name, $record->birthday->format('d F'), "\n"],
+                            $greeting->message
+                        );
+
+                        $response = Http::withHeaders([
+                            'Authorization' => 'Bearer u489f486268ed444.f51e76d509f94b93855bb8bc61521f93'
+                        ])->post('http://46.102.156.214:3001/api/v1/messages', [
+                            'recipient_type' => 'individual',
+                            'to' => $record->whatsapp,
+                            'type' => 'text',
+                            'text' => [
+                                'body' => $message
+                            ]
+                        ]);
+
                         BirthdayLog::create([
                             'id_profile' => $record->id_user,
-                            'status_sent' => 1,
+                            'status_sent' => $response->status() === 200 ? 1 : 0,
                             'date_sent' => now()
                         ]);
 
+                        $this->dispatch('birthday-log-updated');
+
                         Notification::make()
-                            ->title('Ucapan telah terkirim')
-                            ->success()
+                            ->title($response->status() === 200 ?
+                                'Ucapan telah terkirim' :
+                                'Gagal mengirim ucapan')
+                            ->status($response->status() === 200 ? 'success' : 'danger')
                             ->send();
 
                         $this->dispatch('spin-stop');
--- app/Livewire/Filament/Pages/Birthday/TodayTable.php
+++ app/Livewire/Filament/Pages/Birthday/TodayTable.php
@@ -10,6 +10,8 @@ use Filament\Tables\Table;
 use Filament\Tables\Actions\Action;
 use App\Models\BirthdayLog;
 use Filament\Notifications\Notification;
+use Illuminate\Support\Facades\Http;
+use App\Models\BirthdayGreeting;
 
 class TodayTable extends \Filament\Tables\TableComponent
 {
@@ -46,17 +48,38 @@ class TodayTable extends \Filament\Tables\TableComponent
                     ->action(function (Profile $record) {
                         $this->dispatch('spin-start');
 
+                        $greeting = BirthdayGreeting::inRandomOrder()->first();
+
+                        $message = str_replace(
+                            ['{{first_name}}', '{{last_name}}', '{{birthday}}', '\n'],
+                            [$record->first_name, $record->last_name, $record->birthday->format('d F'), "\n"],
+                            $greeting->message
+                        );
+
+                        $response = Http::withHeaders([
+                            'Authorization' => 'Bearer u489f486268ed444.f51e76d509f94b93855bb8bc61521f93'
+                        ])->post('http://46.102.156.214:3001/api/v1/messages', [
+                            'recipient_type' => 'individual',
+                            'to' => $record->whatsapp,
+                            'type' => 'text',
+                            'text' => [
+                                'body' => $message
+                            ]
+                        ]);
+
                         BirthdayLog::create([
                             'id_profile' => $record->id_user,
-                            'status_sent' => 1,
+                            'status_sent' => $response->status() === 200 ? 1 : 0,
                             'date_sent' => now()
                         ]);
 
                         $this->dispatch('birthday-log-updated');
 
                         Notification::make()
-                            ->title('Ucapan telah terkirim')
-                            ->success()
+                            ->title($response->status() === 200 ?
+                                'Ucapan telah terkirim' :
+                                'Gagal mengirim ucapan')
+                            ->status($response->status() === 200 ? 'success' : 'danger')
                             ->send();
 
                         $this->dispatch('spin-stop');
