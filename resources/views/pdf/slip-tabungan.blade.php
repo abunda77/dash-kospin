218919ddf776218c0063bfed273d9ab9964bf60d
Erie Putranto <erieputranto@gmail.com>
2025-01-18 11:42:58 +0700

    update slip setoran awal 2

--- resources/views/pdf/slip-tabungan.blade.php
+++ resources/views/pdf/slip-tabungan.blade.php
@@ -27,7 +27,7 @@
             line-height: 1.5em;
         }
         @page {
-            margin-top: 20px;
+            margin-top: 50px;
             margin-left: 1px;
             margin-right: 1px;
         }
