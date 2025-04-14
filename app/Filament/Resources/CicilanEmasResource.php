6a49b0c19e2340640e4725810e96dc4fae34699d
Erie Putranto <erieputranto@gmail.com>
2025-04-14 11:47:31 +0700

    update: cicilan emas 1- 6 bulan

--- app/Filament/Resources/CicilanEmasResource.php
+++ app/Filament/Resources/CicilanEmasResource.php
@@ -97,6 +97,17 @@ class CicilanEmasResource extends Resource
 
                 Select::make('tenor')
                     ->options([
+                        1 => '1 Bulan',
+                        2 => '2 Bulan',
+                        3 => '3 Bulan',
+                        4 => '4 Bulan',
+                        5 => '5 Bulan',
+                        6 => '6 Bulan',
+                        7 => '7 Bulan',
+                        8 => '8 Bulan',
+                        9 => '9 Bulan',
+                        10 => '10 Bulan',
+                        11 => '11 Bulan',
                         12 => '12 Bulan',
                         24 => '24 Bulan',
                         36 => '36 Bulan',
